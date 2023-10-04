12d2bc6247c1c7b54b64aee4184c5161d8006381
Hemant Purkar <purkar.hemant@gmail.com>
2023-10-04 07:35:13 +0100

    Add files via upload

---
+++ sectionB/loans/src/main/java/com/mybank/loans/mapper/LoansMapper.java
@@ -0,0 +1,28 @@
+package com.mybank.loans.mapper;
+
+import com.mybank.loans.dto.LoansDto;
+import com.mybank.loans.entity.Loans;
+
+public class LoansMapper {
+
+	public static LoansDto mapToLoansDto(Loans loans, LoansDto loansDto) {
+		loansDto.setLoanNumber(loans.getLoanNumber());
+		loansDto.setLoanType(loans.getLoanType());
+		loansDto.setMobileNumber(loans.getMobileNumber());
+		loansDto.setTotalLoan(loans.getTotalLoan());
+		loansDto.setAmountPaid(loans.getAmountPaid());
+		loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+		return loansDto;
+	}
+
+	public static Loans mapToLoans(LoansDto loansDto, Loans loans) {
+		loans.setLoanNumber(loansDto.getLoanNumber());
+		loans.setLoanType(loansDto.getLoanType());
+		loans.setMobileNumber(loansDto.getMobileNumber());
+		loans.setTotalLoan(loansDto.getTotalLoan());
+		loans.setAmountPaid(loansDto.getAmountPaid());
+		loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+		return loans;
+	}
+
+}
