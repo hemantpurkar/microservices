32c6886418dba007c7603002889216586b90acf3
Hemant Purkar <purkar.hemant@gmail.com>
2023-10-04 07:43:13 +0100

    Add files via upload

---
+++ sectionD/loans/src/main/java/com/mybank/loans/repository/LoansRepository.java
@@ -0,0 +1,17 @@
+package com.mybank.loans.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mybank.loans.entity.Loans;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+	Optional<Loans> findByMobileNumber(String mobileNumber);
+
+	Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
