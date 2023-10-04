d9b0b4bed1f526e13724e03907b92518786b7294
Hemant Purkar <purkar.hemant@gmail.com>
2023-10-04 07:39:36 +0100

    Add files via upload

---
+++ sectionC/loans/src/main/java/com/mybank/loans/LoansApplication.java
@@ -0,0 +1,48 @@
+package com.mybank.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.mybank.loans.dto.LoansContactInfoDto;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hemant Purkar",
+						email = "hemantp@example.com",
+						url = "https://www.abc.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.abc.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "MyBank Loans microservice REST API Documentation",
+				url = "https://www.abc.com/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
